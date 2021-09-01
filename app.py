82020197c5e576a27e1f6a2c95fc15310ed03fd7
Arslan Ameer <arslan.ameer@hotmail.com>
2021-09-01 19:25:09 +0500

    ✨Learning: Classes

--- app.py
+++ app.py
@@ -305,3 +305,52 @@
 #     print("Invalid Value")
 # except ZeroDivisionError:
 #     print('age cannot be zero')
+
+# --- CLASSES
+
+# class Point:
+#
+#     def __init__(self, x, y):  # this is constructor
+#         self.x = x
+#         self.y = y
+#
+#     def move(self):
+#         print('move')
+#
+#     def draw(self):
+#         print("draw")
+#
+#
+# point1 = Point(10, 20)
+# print(point1.x)
+
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def talk(self):
+#         print(f"Hi, i am {self.name}")
+#
+# per = Person('basheer')
+# per.talk()
+
+# --- INHERITENCE
+
+# class Mammal:
+#     def walk(self):
+#         print("walk")
+#
+#
+# class Dog(Mammal):
+#     pass
+#
+#
+# class Cat(Mammal):
+#     def annoying(self):
+#         print('annoying')
+#
+#
+# anm1 = Cat()
+# anm1.walk()
+# anm1.annoying()
+
